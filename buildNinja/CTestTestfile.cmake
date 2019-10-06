c92ab6e2c8fc9dcc64af2fbfc4ce84f6f60300f4
radluki <radluki@gmail.com>
2019-10-06 15:04:45 +0200

    Removing build dirs from git

--- buildNinja/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lukasz/cmakeProject
-# Build directory: /home/lukasz/cmakeProject/buildNinja
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
