48e989117dc594ae1bf4de0890713c5dfe5956c1
radzio <lukasz.radzio@nokia.com>
2020-04-05 20:51:06 +0200

    -Wall compilation

--- examples/iteratorCategories.cpp
+++ examples/iteratorCategories.cpp
@@ -15,19 +15,19 @@ public:
   , step_size_{step_size}
   , idx_{idx}
   {}
-  auto operator==(const LinearRangeIterator& lri) const{ 
-    return idx_==lri.idx_; 
+  auto operator==(const LinearRangeIterator& lri) const{
+    return idx_==lri.idx_;
   }
-  auto operator!=(const LinearRangeIterator& lri) const{ 
-    return !(*this==lri); 
+  auto operator!=(const LinearRangeIterator& lri) const{
+    return !(*this==lri);
   }
   auto& operator++() { ++idx_; return *this; }
   auto& operator--() { --idx_; return *this; }
   auto operator*() const { return start_ + (idx_*step_size_); }
 private:
-  size_t idx_{};
   T start_{};
   T step_size_{};
+  size_t idx_{};
 };
 
 int main()
@@ -35,7 +35,9 @@ int main()
     LinearRangeIterator<int> it{0, 10, 0};
     using ItType = LinearRangeIterator<int>;
     static_assert(std::is_same<std::iterator_traits<ItType>::iterator_category, std::bidirectional_iterator_tag>::value, "");
-    for(it; it != LinearRangeIterator<int>{0, 10, 4}; ++it)
+    for(; it != LinearRangeIterator<int>{0, 10, 4}; ++it)
+    {
         LOG << *it;
+    }
     return 0;
 }
